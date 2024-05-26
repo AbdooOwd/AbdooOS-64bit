03bc76ea5fe14895a0ca603cb9e647d39dcbf77e
AbdooOwd <benari102@gmail.com>
2024-05-26 20:10:41 +0100

    Made kernel a bit fancier

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,24 +3,29 @@
 #include "../drivers/screen.h"
 
 #include "../lib/util.h"
+#include "../lib/disk.h"
 
+#include "../cpu/cpu.h"
 
 
 bool found_fat = false;
 
 void init_kernel() {
 
-    // put here whatever you cannot initialize outside a function
-
     kernel_main();
+    
 }
 
 void kernel_main() {
     print("Booted AbdooOS Successfully!\n");
     print("Searching for FAT32...\n");
 
+    search_fat();
+}
+
+void search_fat() {
     if (found_fat) {
-        print("Found FAT!\n");
+        success("Found FAT!\n");
     } else {
         err("ERROR: FAT NOT FOUND\n");
         halt();
