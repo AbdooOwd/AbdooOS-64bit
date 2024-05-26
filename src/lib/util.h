43aad6b10fcba4a4a85e80497021ee00418dc2b2
AbdooOwd <benari102@gmail.com>
2024-05-26 20:10:00 +0100

    removed halt

--- src/lib/util.h
+++ src/lib/util.h
@@ -12,12 +12,7 @@ void array_copy(unsigned char* source, unsigned char* dest);
 char* int_to_str(int num);
 
 
-static void halt(void) __attribute__((unused));
-static void halt(void) {
-    asm ("cli");
-    for (;;) {
-        asm ("hlt");
-    }
-}
+#define min(a,b)    ((a) < (b) ? (a) : (b))
+#define max(a,b)    ((a) > (b) ? (a) : (b))
 
 #endif
