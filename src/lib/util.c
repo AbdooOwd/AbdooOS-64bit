a54fe6f534668e8d765021d58b65bc743cc2ff1d
AbdooOwd <benari102@gmail.com>
2024-05-29 18:48:08 +0100

    removed random cuz no GDT

--- src/lib/util.c
+++ src/lib/util.c
@@ -1,14 +1,7 @@
 #include "util.h"
-#include "../kernel/gdt.h"
 
 void array_copy(unsigned char* source, unsigned char* dest) {
     for (size_t i = 0; i < sizeof(source); i++) {
         dest[i] = source[i];
     }
-}
-
-// works when KASLR on
-int random() {
-    // haha playing with addresses to generate a random number until I make a timer
-    return (u8) ((gdt_addr >> 16) / 3 + 5 * (4 - 2 / ((u8) gdt_init)) - 12);
 }
