76b723a11626cc35a45c31e5c2c4e02b54272fa5
AbdooOwd <benari102@gmail.com>
2024-05-30 20:53:02 +0100

    silly padding

--- src/include/types.h
+++ src/include/types.h
@@ -9,16 +9,18 @@
 
 
 // Ah yes short typenames
-typedef uint8_t  u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
+typedef uint8_t     u8;
+typedef uint16_t    u16;
+typedef uint32_t    u32;
+typedef uint64_t	u64;
 
-typedef int8_t  i8;
-typedef int16_t i16;
-typedef int32_t i32;
-typedef int64_t i64;
+typedef int8_t      i8;
+typedef int16_t     i16;
+typedef int32_t     i32;
+typedef int64_t     i64;
 
-typedef char* string;
+typedef u64     	addr_t;		// cuz we're in 64bits
+
+typedef char*       string;
 
 #endif
