4cad1ee684085da040682fb606679931ae8deedf
AbdooOwd <benari102@gmail.com>
2024-06-01 13:29:41 +0100

    just realizd that i had functions i wasnt using

--- src/kernel/io.c
+++ src/kernel/io.c
@@ -20,20 +20,3 @@ unsigned short inw(unsigned short port) {
 void outw(unsigned short port, unsigned short data) {
     asm volatile(" out %%ax , %% dx" : : "a" (data), "d" (port));
 }
-
-static inline void outb(u16 port, u8 val) {
-    asm volatile ("outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
-    /* There's an outb %al, $imm8 encoding, for compile-time constant port numbers that fit in 8b. (N constraint).
-     * Wider immediate constants would be truncated at assemble-time (e.g. "i" constraint).
-     * The  outb  %al, %dx  encoding is the only option for all other cases.
-     * %1 expands to %dx because  port  is a u16.  %w1 could be used if we had the port number a wider C type */
-}
-
-static inline u8 inb(u16 port) {
-    u8 ret;
-    asm volatile ("inb %w1, %b0"
-        : "=a"(ret)
-        : "Nd"(port)
-        : "memory");
-    return ret;
-}
--- src/kernel/io.h
+++ src/kernel/io.h
@@ -10,7 +10,4 @@ void outb(unsigned short port, unsigned char data);
 unsigned short inw(unsigned short port);
 void outw(unsigned short port, unsigned short data);
 
-static inline void outb(u16 port, u8 val);
-static inline u8 inb(u16 port);
-
 #endif
