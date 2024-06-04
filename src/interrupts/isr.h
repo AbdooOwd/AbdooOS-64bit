e0355dcf34d8382e11e194241fe6a3d94b272356
AbdooOwd <benari102@gmail.com>
2024-06-04 19:58:49 +0100

    fokq interrupts

--- src/interrupts/isr.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <include/types.h>
-#include <cpu/cpu.h>
-
-
-execution_context* ISR_handler(execution_context* regs);
-void ISR_initGates();
--- src/kernel/hal/hal.c
+++ src/kernel/hal/hal.c
@@ -1,9 +1,7 @@
 #include "hal.h"
 #include <kernel/gdt.h>
-#include <interrupts/idt.h>
 #include <cpu/cpu.h>
 
 void HAL_init() {
     GDT_init();
-    IDT_init();
 }
