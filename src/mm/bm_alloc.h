812764304357390c388d2fefa45275474b44f309
AbdooOwd <benari102@gmail.com>
2024-06-18 17:17:57 +0100

    more memory yeaaa

--- src/mm/bm_alloc.h
+++ src/mm/bm_alloc.h
@@ -10,7 +10,7 @@
 
 
 #define MEMORY_SIZE 1024 * 1024 * 64 			// Total memory managed by the allocator (64MB)
-#define BLOCK_SIZE 	1024 * 1024         		// Size of each memory block (1MB)
+#define BLOCK_SIZE 	1024 * 128         			// Size of each memory block (128KiB)
 #define NUM_BLOCKS 	(MEMORY_SIZE / BLOCK_SIZE) 	// Total number of blocks
 
 // Utility macros for setting and checking bits in the bitmap
@@ -18,6 +18,9 @@
 #define BIT_CLEAR(bitmap, n) (bitmap[(n) / 8] &= ~(1 << ((n) % 8)))
 #define BIT_CHECK(bitmap, n) (bitmap[(n) / 8] & (1 << ((n) % 8)))
 
+
+extern u8 memory[MEMORY_SIZE]; // The memory array
+extern u8 bitmap[NUM_BLOCKS / 8]; // The bitmap array
 
 // functions
 
